13baa58682009d4935bcba8ed4fb019e96fe3c0f
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-25 16:29:22 +0330

    Add files via upload

---
+++ WindowsFormsApp35/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp35
+{
+    class cEmployee
+    {
+        public string identity;
+        public string firstname;
+        public string lastname;
+
+        public string Concatinate()
+        {
+            return firstname + ' ' + lastname;
+        }
+
+    }
+}
---
+++ WindowsFormsApp35/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp35
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cEmployee sam = new cEmployee();
+            sam.firstname = "ali";
+            sam.lastname = "rezaee";
+            sam.identity = "002745632";
+            label1.Text = sam.Concatinate();
+        }
+    }
+}
